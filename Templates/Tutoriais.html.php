5779063280c9f91fbe3b0fb56e8a35c44ae91ec8
francisco <frncsc0ferreira@gmail.com>
2018-12-12 16:44:05 +0000

    + tutorias

--- Templates/Tutoriais.html.php
+++ Templates/Tutoriais.html.php
@@ -1,68 +1,24 @@
 
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="">Simple Ecommerce</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<!--  bread crump -->
 
-        <div class="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault">
-            <ul class="navbar-nav m-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="">Home</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="">Categories <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Product</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Cart</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Contact</a>
-                </li>
-            </ul>
+<!---->
+<!--<div class="container">-->
+<!--    <div class="row">-->
+<!--        <div class="col">-->
+<!--            <nav aria-label="breadcrumb">-->
+<!--                <ol class="breadcrumb">-->
+<!--                    <li class="breadcrumb-item"><a href="">Home</a></li>-->
+<!--                    <li class="breadcrumb-item"><a href="">Category</a></li>-->
+<!--                    <li class="breadcrumb-item active" >Sub-category</li>-->
+<!--                </ol>-->
+<!--            </nav>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+<!---->
 
-            <form class="form-inline my-2 my-lg-0">
-                <div class="input-group input-group-sm">
-                    <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Search...">
-                    <div class="input-group-append">
-                        <button type="button" class="btn btn-secondary btn-number">
-                            <i class="fa fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-                <a class="btn btn-success btn-sm ml-3" href="">
-                    <i class="fa fa-shopping-cart"></i> Cart
-                    <span class="badge badge-light">3</span>
-                </a>
-            </form>
-        </div>
-    </div>
-</nav>
-<section class="jumbotron text-center">
-    <div class="container">
-        <h1 class="jumbotron-heading">E-COMMERCE CATEGORY</h1>
-        <p class="lead text-muted mb-0">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte...</p>
-    </div>
-</section>
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="">Home</a></li>
-                    <li class="breadcrumb-item"><a href="">Category</a></li>
-                    <li class="breadcrumb-item active" >Sub-category</li>
-                </ol>
-            </nav>
-        </div>
-    </div>
-</div>
+
 <div class="container">
     <div class="row">
         <div class="col-12 col-sm-3">
--- masterPage.html.php
+++ masterPage.html.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="Assets/bootstrap-4.1.3-dist/css/bootstrap.css" >
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="Assets/css/style.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
     <style>
     </style>
@@ -21,18 +22,25 @@
 
 <?php require_once "Templates/TemplateHeader.html.php"?>
 
-<!--main-->
+<!--main    pagina inicial -->
 <?php //require_once "Templates/default.html.php"?>
 
 <!--authors-->
 <?php // require_once "Templates/TemplateAuthorsList.html.php"?>
 
 
+<!--tutoriais -->
 <?php require_once "Templates/Tutoriais.html.php"?>
+
+
+
 
 <!-- login/regist -->
 <?php require_once "Templates/TemplateLogin" ?>
 
+
+
+<!-- footer -->
 <?php require_once "Templates/TemplateFooter.html.php"?>
 
 
