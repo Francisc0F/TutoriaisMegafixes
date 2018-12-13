86422f9b9ab25e273a75e692a4860ae68172ea8d
francisco <frncsc0ferreira@gmail.com>
2018-12-13 12:28:15 +0000

    + tutorias 12:27 13/12

--- Templates/Tutoriais.html.php
+++ Templates/Tutoriais.html.php
@@ -34,49 +34,146 @@
                 </ul>
             </div>
 
-         <!-- most relevant item-->
-<!--            <div class="card bg-light mb-3">-->
-<!--                <div class="card-header bg-success text-white text-uppercase">Last product</div>-->
-<!--                <div class="card-body">-->
-<!--                    <img class="img-fluid" src="https://dummyimage.com/600x400/55595c/fff" />-->
-<!--                    <h5 class="card-title">Product title</h5>-->
-<!--                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-<!--                    <p class="bloc_left_price">99.00 $</p>-->
-<!--                </div>-->
-<!--            </div>-->
+            <!-- most relevant item-->
+            <!--            <div class="card bg-light mb-3">-->
+            <!--                <div class="card-header bg-success text-white text-uppercase">Last product</div>-->
+            <!--                <div class="card-body">-->
+            <!--                    <img class="img-fluid" src="https://dummyimage.com/600x400/55595c/fff" />-->
+            <!--                    <h5 class="card-title">Product title</h5>-->
+            <!--                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+            <!--                    <p class="bloc_left_price">99.00 $</p>-->
+            <!--                </div>-->
+            <!--            </div>-->
 
         </div>
 
 
         <!-- items by categories -->
-        <div class="col">
+        <div class="col" id="categories">
             <div class="row ">
+<!--              foreach cat-->
+                        <?php for($i = 0 ; $i<6;$i++) { ?>
+                    <div class="col-12 col-md-6 col-lg-4 <?php if($i > 2) echo "mt-3" ?> category-pages">
+                        <div class="card ">
 
-                <?php for($i = 0 ; $i<6;$i++) { ?>
-                <div class="col-12 col-md-6 col-lg-4 <?php if($i > 2) echo "mt-3" ?>">
-                    <div class="card ">
-                        <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap">
-                        <div class="card-body p-3 position-relative">
+                            <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap">
+
+                            <div class="card-body p-3 position-relative">
 
 
-                            <h4 class="card-title mb-1"><a href="" title="View Product">Product title</a> </h4>
+                                <h4 class="card-title mb-1 ">
+                                    <a href="" title="View Product">Product title</a>
+                                </h4>
 
 
-                            <small class="upload-time" >3 days ago</small>
-                            <p class="card-text description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <div class="row">
-                                <div class="col">
-                                    <p class="btn btn-danger btn-block">99.00 $</p>
+                                <small class="upload-time" >3 days ago</small>
+
+                                <p class="card-text description">Some
+                                    quick example text to build on the card title
+                                    and make up the bulk of the card's content.</p>
+
+                                <div class="row">
+
+
+                                    <div class="col">
+                                        <div class="group d-flex justify-content-between mt-2">
+
+                                            <div class="rate w-100 d-flex justify-content-start">
+                                                <span class="fa fa-star checked "></span>
+                                                <span class="fa fa-star checked ml-1"></span>
+                                                <span class="fa fa-star checked ml-1"></span>
+                                                <span class="fa fa-star ml-1" ></span>
+                                                <span class="fa fa-star ml-1"></span>
+                                            </div>
+
+
+                                            <span class="text-warning ml-0 dificulty">Intermediate</span>
+
+                                        </div>
+                                    </div>
+
+
                                 </div>
-                                <div class="col">
-                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>
-                                </div>
+
+
+                                <!--                            <p class="card-text description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+                                <!--                            <div class="row">-->
+                                <!--                                <div class="col">-->
+                                <!--                                    <p class="btn btn-danger btn-block">99.00 $</p>-->
+                                <!--                                </div>-->
+                                <!--                                <div class="col">-->
+                                <!--                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>-->
+                                <!--                                </div>-->
+                                <!--                            </div>-->
                             </div>
                         </div>
                     </div>
-                </div>
 
                 <?php } ?>
+
+            <!-- categorie 2 -->
+
+                <?php for($i = 0 ; $i<6;$i++) { ?>
+                    <div class="col-12 col-md-6 col-lg-4 <?php if($i > 2) echo "mt-3" ?> category-pages ">
+                        <div class="card ">
+
+                            <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap">
+
+                            <div class="card-body p-3 position-relative">
+
+
+                                <h4 class="card-title mb-1 ">
+                                    <a href="" title="View Product">Product title</a>
+                                </h4>
+
+
+                                <small class="upload-time" >10 days ago</small>
+
+                                <p class="card-text description">Some
+                                    quick example text to build on the card title
+                                    and make up the bulk of the card's content.</p>
+
+                                <div class="row">
+
+
+                                    <div class="col">
+                                        <div class="group d-flex justify-content-between mt-2">
+
+                                            <div class="rate w-100 d-flex justify-content-start">
+                                                <span class="fa fa-star checked "></span>
+                                                <span class="fa fa-star checked ml-1"></span>
+                                                <span class="fa fa-star checked ml-1"></span>
+                                                <span class="fa fa-star ml-1" ></span>
+                                                <span class="fa fa-star ml-1"></span>
+                                            </div>
+
+
+                                            <span class="text-warning ml-0 dificulty">Intermediate</span>
+
+                                        </div>
+                                    </div>
+
+
+                                </div>
+
+
+                                <!--                            <p class="card-text description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+                                <!--                            <div class="row">-->
+                                <!--                                <div class="col">-->
+                                <!--                                    <p class="btn btn-danger btn-block">99.00 $</p>-->
+                                <!--                                </div>-->
+                                <!--                                <div class="col">-->
+                                <!--                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>-->
+                                <!--                                </div>-->
+                                <!--                            </div>-->
+                            </div>
+                        </div>
+                    </div>
+
+                <?php } ?>
+
+
+
 
                 <!-- pagination -->
                 <div class="col-12">
