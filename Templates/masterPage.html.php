e28e5193672abef8547c385d1fabfc2b5393a963
francisco <frncsc0ferreira@gmail.com>
2018-12-14 15:33:39 +0000

    + laravel master pg

--- Templates/masterPage.html.php
+++ Templates/masterPage.html.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <title>Ex23</title>
 
-    <link rel="stylesheet" href="resources/assets/bootstrap-4.1.3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/resources/assets/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="resources/assets/css/style.css">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/resources/assets/">
+
 
     <!-- Include Editor style. -->
     <link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_editor.pkgd.min.css" rel="stylesheet"
@@ -46,8 +45,8 @@
 <?php require_once "Templates/TemplateFooter.html.php" ?>
 
 
-<script src="resources/assets/js/jquery.min.js"></script>
-<script src="resources/assets/js/app.js"></script>
+<script src="/resources/assets/js/jquery.min.js"></script>
+<script src="/resources/js/app.js"></script>
 
 <script type="text/javascript"
         src="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/js/froala_editor.pkgd.min.js"></script>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="/resources/assets/bootstrap/dist/css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="/resources/assets/">
+
+
+        <!-- Include Editor style. -->
+        <link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_editor.pkgd.min.css" rel="stylesheet"
+              type="text/css"/>
+        <link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_style.min.css" rel="stylesheet"
+              type="text/css"/>
+
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+
+
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="/resources/js/app.js"></script>
+
+    <script type="text/javascript"
+            src="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/js/froala_editor.pkgd.min.js"></script>
+
+
+    </body>
+</html>
