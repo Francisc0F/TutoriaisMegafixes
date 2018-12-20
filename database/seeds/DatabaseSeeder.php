5ba334458c5d53683fc9655725b45e2c6f7320e1
francisco <frncsc0ferreira@gmail.com>
2018-12-20 19:22:13 +0000

    + Controlador user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-         $this->call(UtilizadorTableSeeder::class);
+         //$this->call(UtilizadorTableSeeder::class);
     }
+
 }
