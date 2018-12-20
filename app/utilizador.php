1c171fb8dade799d66e8ce5bf88d8ef36318e25a
francisco <frncsc0ferreira@gmail.com>
2018-12-20 18:11:45 +0000

    + refreshed migrations

---
+++ app/utilizador.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class utilizador extends Model
+{
+    protected $fillable= ["tipo_utilizador","nome_utilizador","num_tutoriais","password_utilizador","email_utilizador"];
+
+
+
+
+}
---
+++ database/migrations/2018_12_20_173040_create_utilizadors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUtilizadorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilizadors', function (Blueprint $table) {
+            $table->increments('id_utilizador');
+            $table->string('tipo_utilizador');
+            $table->string('nome_utilizador');
+            $table->integer('num_tutoriais');
+            $table->string('password_utilizador');
+            $table->string('email_utilizador');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilizadors');
+    }
+}
