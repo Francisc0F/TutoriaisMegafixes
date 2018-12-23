d6cf29e6b92a54c82157d37bb691dc5e34faeba2
francisco <frncsc0ferreira@gmail.com>
2018-12-23 15:40:07 +0000

    + migration tutoriais

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
          $this->call(UtilizadorTableSeeder::class);
          $this->call(CategoriasTableSeeder::class);
+         $this->call(UtilizadorTableSeeder::class);
     }
 
 }
